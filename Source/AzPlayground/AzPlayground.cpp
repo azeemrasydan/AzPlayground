0ed09fec8ac38fe1bc3fd57ccd2f8c5c410ea189
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-02-12 16:39:43 +0800

    First commit

---
+++ Source/AzPlayground/AzPlayground.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AzPlayground.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AzPlayground, "AzPlayground" );
